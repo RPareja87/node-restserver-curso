efb8454b8057161e3779f65c68380441b7fd602a
Ronald <ronald.pareja87@gmail.com>
2020-05-23 00:22:41 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,26 @@
 // Puerto
 // =======================================
 process.env.PORT = process.env.PORT || 3000;
+
+
+//user: strider
+//pass: MGca1Gc9cVy3bJyg
+
+//mongodb ://strider:MGca1Gc9cVy3bJyg@cafe-ctyoi.mongodb.net/cafe
+// =======================================
+// Entorno
+// =======================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+console.log(process.env.NODE_ENV);
+// =======================================
+// Base de Datos
+// =======================================
+let urlDB;
+
+if (!process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://strider:MGca1Gc9cVy3bJyg@cafe-ctyoi.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB;

b5b8bd6636c18863bea9a28f6af676c69c9501db
Ronald <ronald.pareja87@gmail.com>
2020-06-01 00:14:59 -0500

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -7,18 +7,31 @@ process.env.PORT = process.env.PORT || 3000;
 //user: strider
 //pass: MGca1Gc9cVy3bJyg
 
-//mongodb ://strider:MGca1Gc9cVy3bJyg@cafe-ctyoi.mongodb.net/cafe
+//mongodb://strider:MGca1Gc9cVy3bJyg@cafe-ctyoi.mongodb.net/cafe
 // =======================================
 // Entorno
 // =======================================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 console.log(process.env.NODE_ENV);
 // =======================================
+// Vencimiento del token
+// =======================================
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// =======================================
+// Seed de autenticacion
+// =======================================
+
+process.env.SEED = process.env.SEED || 'este-es-el-seet-desarrollo';
+
+
+// =======================================
 // Base de Datos
 // =======================================
 let urlDB;
 
-if (!process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     urlDB = process.env.MONGO_URI;
